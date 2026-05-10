d3e53c1f570b5a78069b3d91f6fdd748a14874e7
rishabh6395 <rishabhbhatt956@gmail.com>
2026-05-10 19:19:23 +0530

    intro of arrays, 2D pending

---
+++ Lec 9/main.java
@@ -0,0 +1,22 @@
+// Arrays in Java
+
+public class main {
+    public static void main(String[] args) {
+        int[] rollNum = new int[3];
+        // 101, 102, 103
+
+        rollNum[0] = 101;
+        rollNum[1] = 102;
+        rollNum[2] = 103;
+
+        // for(int i =0; i<rollNum.length; i++){
+        //     System.out.println("arrays : "+ rollNum[i]);
+        // }
+        // System.out.println(rollNum[0]);
+
+        // 2D arrays:
+
+        
+        
+    }
+}

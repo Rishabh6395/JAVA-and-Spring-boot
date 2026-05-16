a2c5fd40e596cbe5878807df9ed3322ebda5fa33
rishabh6395 <rishabhbhatt956@gmail.com>
2026-05-16 19:09:25 +0530

    arrays in Java

---
+++ DSA in JAVA/Lec 13/main.java
@@ -0,0 +1,78 @@
+// Arrays in Java
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        
+        // declaration of an array:
+
+        // int arr[];
+
+        // allocation
+        // arr = new int[5];
+
+        // init
+        int brr[] = {1,2,3};
+        // System.out.println("single value priniting: "+brr[1]);
+
+        // for(int i = brr.length - 1; i >= 0; i--){
+        //     System.out.println(brr[i]);
+        // }
+
+        // For Each Loop
+        // for(int val: brr){
+        //     System.out.println(val);
+        // }
+
+        // how to take input in array:
+
+        /*
+            Normal way of taking input in array:
+            Scanner sc = new Scanner(system.in)
+            int n;
+            n = sc.nextInt()
+        */
+
+        // int arr[] = new int[5];
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter the element");
+        // for(int i =0; i<=arr.length -1; i++){
+        //     System.out.println("Provide the element value: " + i);
+        //     arr[i] = sc.nextInt();
+        // }
+
+        // for(int val: arr){
+        //     System.out.println("printing n values :"+val);
+        // }
+
+        // Printing the sum of an array:
+
+        // int arr[] = {10,20,30,40,50};
+        // int sum = 0;
+
+        // for(int i =0; i<= arr.length-1; i++){
+        //     sum += arr[i];
+        // }
+        // System.out.println("Sum of an array is: " +sum);
+
+        // FIND MAXIMUM ELEMENT IN AN ARRAY:
+
+        int arr[] = {1,234,232,12,-12, 2,76,34};
+
+        int maxi = arr[0];
+
+        for(int i = 0; i<= arr.length -1; i++){
+            if(arr[i] > maxi){
+                maxi = arr[i];
+            }
+        }
+        System.out.println(maxi);
+
+
+
+
+
+    }
+}

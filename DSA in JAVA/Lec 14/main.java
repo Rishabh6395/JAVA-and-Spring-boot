4e7325f40d15b16139326c017b9f78b66d902a38
rishabh6395 <rishabhbhatt956@gmail.com>
2026-05-24 08:40:53 +0530

    some questions done

--- DSA in JAVA/Lec 14/main.java
+++ DSA in JAVA/Lec 14/main.java
@@ -4,22 +4,73 @@ import java.util.Scanner;
 
 public class main {
 
+    // 1. Print each char of the string;
     static void printString(String str){
         for(int i =0; i<str.length(); i++){
             char l = str.charAt(i);
             System.out.println(l);
         }
-    }
+    };
 
+    // 2. count length of string without using length()
     static int getLength(String str){
         char[] arr = str.toCharArray();
         int len = arr.length;
         return len;
+    };
+
+    
+    // 3. count vowels in a string;
+    static void getVowelsCount(String str){
+        int count = 0;
+        for(int i =0; i<=str.length()-1; i++){
+            char ch = str.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' ||ch == 'o' || ch == 'u'){
+                count++;
+            }
+        }
+        System.out.println(count);
+    };
+
+    // 4. Reverse a string;
+
+    static void printReverseStr(String str){
+        String newStrr = "";
+        for(int  i = str.length()-1; i>=0; i--){
+            // System.out.println(str.charAt(i));
+            newStrr += str.charAt(i);
+        }
+        System.out.println(newStrr);
+    };
+
+    // 5. check string is a palindrome or not; 
+    
+    static void checkPalindrom(String str){
+        String temp = "";
+        String newTemp = "";
+        for(int i =0; i<= str.length()-1; i++){
+            temp += str.charAt(i);
+            // System.out.println("Temp: " + temp);
+        }
+        for(int j = str.length()-1; j>=0; j--){
+            newTemp += str.charAt(j);
+            // System.out.println("newTemp: " + newTemp);
+        }
+        if(temp.equals(newTemp)){
+            System.out.println("Yes");
+        } 
+        else{
+            System.out.println("No");
+        }
     }
 
     public static void main(String[] args) {
 
-        System.out.println(getLength("rishabh")); 
+        // System.out.println(getLength("rishabh")); 
+        // System.out.println(getVowelsCount("Rishabh"));
+        // getVowelsCount("my name is rishabh bhatt");
+        // printReverseStr("Rishabh");
+        checkPalindrom("mom");
 
         // String fName = "rishabh";
         // String lName = "bhatt";

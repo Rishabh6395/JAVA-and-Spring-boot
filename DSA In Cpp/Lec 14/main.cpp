6e726b5298139560964b1beb30251e5f3a8f1ef6
rishabh6395 <rishabhbhatt956@gmail.com>
2026-06-15 04:00:07 +0530

    pivot element in array

---
+++ DSA In Cpp/Lec 14/main.cpp
@@ -0,0 +1,32 @@
+// pivot element in array;
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findPivot(int arr[], int n){
+    int start = 0;
+    int end = n -1;
+    int mid = start + (end - start)/2;
+
+    while(start< end){
+        if(arr[mid] >= arr[0]){
+            start = mid + 1;
+        }
+        else{
+            end = mid;
+        }
+        mid = start + (end - start)/2;
+    }
+    return start;
+}
+
+
+int main(){
+
+    int arr[5] = {3, 8,10,17,1};
+
+    cout<<"pivot is: "<<findPivot(arr, 5);
+
+    return 0;
+}

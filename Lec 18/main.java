2db8d51520b2fbaf15fe4056cc56230488b6b525
rishabh6395 <rishabhbhatt956@gmail.com>
2026-08-03 04:25:17 +0530

    autoboxing and unboxing.. pending

---
+++ Lec 18/main.java
@@ -0,0 +1,19 @@
+
+
+public class main {
+    public static void main(String[] args) {
+        // Autoboxing
+        int x = 10;
+        Integer y = x;
+
+        System.out.println(x);
+        System.out.println(y); // unboxing
+
+        // unboxing
+        Integer a = 20; // autoboxing
+        int b = a; // unboxing
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
